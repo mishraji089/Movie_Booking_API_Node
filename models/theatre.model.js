49d9c0e23e661808fa321d51b4c62dad6fdc8f0a
dhruv <mishradhruv089@gmail.com>
2025-12-07 11:34:21 +0530

    made a Theatre Schema and model

---
+++ models/theatre.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const theatreSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        
+    },
+    description: String,
+
+    city:{
+        type:String,
+        required:true
+    },
+    pincode:{
+        type:number,
+        required:true
+    },
+    address: String
+
+},{timestamps:true});
+
+const Theatre = mongoose.model('Theatre', theatreSchema); //creates a new model
+
+module.exports = Theatre; //returning a model
+
