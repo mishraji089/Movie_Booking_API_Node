bd7461a19dde61682ea873905df16049bc8edb73
dhruv <mishradhruv089@gmail.com>
2025-12-13 13:06:13 +0530

    Added filter in getting all movies

--- services/theatre.service.js
+++ services/theatre.service.js
@@ -47,10 +47,24 @@ const getTheatre = async (id) => {
 }
 
 
-const getAllTheatres = async () => {
+const getAllTheatres = async (data) => {
 
     try {
-        const response = await Theatre.find({});
+        let query={};
+        if(data && data.city){
+            query.city=data.city;
+        }
+        if(data && data.pincode){
+             query.pincode=data.pincode;
+
+        }
+
+        if(data && data.name){
+            //this checks whether name is present in quesry params or not
+            query.name=data.name;
+        }
+        
+        const response = await Theatre.find(query);
         return response;
     } catch (error) {
         console.log(error);
