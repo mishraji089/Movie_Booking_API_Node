d5746e88ca7a2b2c73f43ce0cc86ee92e15fa4bb
dhruv <mishradhruv089@gmail.com>
2026-01-09 16:51:30 +0530

    added basic middeware to auth

--- services/user.service.js
+++ services/user.service.js
@@ -6,6 +6,14 @@ const createUser=async (data)=>{
         return response;
     } catch (error) {
         console.log(error);
+        if(error.name=='ValidationError'){
+            let err={};
+            Object.keys(error.errors).forEach((key)=>{
+
+                err[key]=error.errors[key].message;
+            });
+            throw {err:err,code:422};
+        }
         throw error;
     }
 }
